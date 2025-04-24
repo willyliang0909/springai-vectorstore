6338121f7099721d6177115ead9eb37896821c97
willyliang <willy_liang@ecomsoft.com.tw>
2025-04-24 15:31:30 +0800

    init

---
+++ src/main/java/com/test/springai/vectorstore/VectorstoreApplication.java
@@ -0,0 +1,13 @@
+package com.test.springai.vectorstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/springai/vectorstore/VectorstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springai.vectorstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
